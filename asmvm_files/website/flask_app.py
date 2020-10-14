638755cf8aa65113c2e02990f879de26b6f9b0e2
ASMVM <66266053+asmvm@users.noreply.github.com>
2020-10-13 22:13:00 -0400

    new path to mongodb in flask app

--- asmvm_files/website/flask_app.py
+++ asmvm_files/website/flask_app.py
@@ -20,16 +20,23 @@ app.config['MONGO_URI'] = "mongodb://localhost:27017/Dodecagon"
 
 mongo = PyMongo(app)
 
-@app.route("/")
-def index():
-    meal = mongo.db.recipes.find_one()
-    return render_template(index.html, meal=meal)
+from pymongo import MongoClient
+client = MongoClient('mongodb://localhost:27017/')
+db = client['Dodecagon']
+recipes_collection = db.recipes
 
-# @app.route('/dish/<dish>')
-# def  dish():
-#     meal = mongo.db.recipes.findOne({},{'Title':dish})
-#     resp = dumps(dish)
-#     return resp
+
+# @app.route("/")
+# def index():
+#     meal = mongo.db.recipes.find_one()
+#     return render_template(index.html, meal=meal)
+
+@app.route('/dish/<dish>')
+def  dish(dish):
+    meal = recipes_collection.find({'Title':dish})
+    print(meal)
+    resp = dumps(meal)
+    return resp
 
 
 @app.route('/recipes')
@@ -38,6 +45,11 @@ def recipes():
     resp = dumps(recipes)
     return resp
 
+@app.route('/wines')
+def wines():
+    wines = mongo.db.wines.find({},{'wine':1})
+    resp = dumps(wines)
+    return resp
 
 
 if __name__ == "__main__":
