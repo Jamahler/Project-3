a9a039c877345b3df265ef7b4f74451e61dff5d2
ASMVM <66266053+asmvm@users.noreply.github.com>
2020-10-20 11:32:54 -0400

    flask app file re-organized

--- asmvm_files/website/flask_app.py
+++ asmvm_files/website/flask_app.py
@@ -1,18 +1,12 @@
 from flask import Flask, render_template, request
 import requests
 import pymongo
-# from flask_pymongo import PyMongo
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from flask import jsonify, request
-from werkzeug.security import generate_password_hash, check_password_hash
 from bson import json_util
 
-
-
 app = Flask(__name__)
-
-# app.secret_key = "secretkey"
 
 # app.config['MONGO_URI'] = "mongodb://localhost:27017/Dodecagon"
 # mongo = PyMongo(app)
@@ -21,11 +15,72 @@ app = Flask(__name__)
 # db = client['Dodecagon']
 # recipes_collection = db.recipes
 
+# Suggested format to connect with MongoDB 
 from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017/')
 db = client['Dodecagon']
 recipes_collection = db.recipes
 wines_collection = db.wines
+
+
+# This route connects with mongo db and gets all recipes
+@app.route('/recipe')
+def getrecipe():
+    # wines_data = [x for x in wines_collection.find()]
+    recipe = recipes_collection.find()
+    # resp = dumps(wines[2]['wine'])
+    # resp = dumps(wines)
+    # resp = json_util.dumps(wine for wine in wines)
+
+    resp = json_util.dumps(recipe)
+ 
+    # return render_template('wines.html', wines=resp)    
+    return resp
+
+
+# This route returns a loop of the wine list, wish to use this for dynamic dropdown
+# However, when we render to template, we only get one
+# character at a time, not the full wine name
+@app.route('/winess')
+def winelist():
+    # wines_data = [x for x in wines_collection.find()]
+    wines = wines_collection.find()
+    
+
+    # for w in len(wines):
+    #     x = wines[w]['wine']      
+    #     print(x)
+    # resp = dumps(wines[2]['wine'])
+    # resp = json_util.dumps(wines)
+    resp = json_util.dumps(wine for wine in wines)
+    # resp = json_util.dumps({'wine':wines[1]['wine']})
+
+    # print(wines)
+    return render_template('wine_dropdown.html', wines=resp)    
+    # return resp
+
+# THIS IS THE GOOD JSON ROUTE DO NOT DELETE!
+# Alternative format to get wine information, as all previous attempts only
+# loop through each character in the wine names when we render to template
+@app.route('/json-wines', methods=['GET', 'POST'])
+def get_all_wines():
+  wines = wines_collection.find()
+  output = []
+
+  for w in wines:
+    output.append({'wine':w['wine'], 'pairing':w['pairing'], 'description':w['description']})
+  print(output)
+  wines_json = jsonify(output)
+  return wines_json
+  
+#   return render_template('wines.html', wines=list)  
+#   return jsonify({'result': output[2]["wine"]})
+#   print(output)
+#   return jsonify({'result': output})
+#   return jsonify(list_of_dictionaries)
+
+## Additional routes we tested to try and loop through wines or recipes in MongoDB
+
 
 # @app.route("/")
 # def index():
@@ -66,61 +121,13 @@ wines_collection = db.wines
 #     resp = dumps(wine, pairing)
 #     return resp
 
-# This route returns a loop of the wine list
-@app.route('/winess')
-def winelist():
-    # wines_data = [x for x in wines_collection.find()]
-    wines = wines_collection.find()
-    
-
-    # for w in len(wines):
-    #     x = wines[w]['wine']      
-    #     print(x)
-    # resp = dumps(wines[2]['wine'])
-    # resp = json_util.dumps(wines)
-    resp = json_util.dumps(wine for wine in wines)
-    # resp = json_util.dumps({'wine':wines[1]['wine']})
-
-    # print(wines)
-    return render_template('wine_dropdown.html', wines=resp)    
-    # return resp
-
-# THIS IS THE GOOD JSON ROUTE DO NOT DELETE!!!
-@app.route('/json-wines', methods=['GET', 'POST'])
-def get_all_wines():
-  wines = wines_collection.find()
-  output = []
-
-  for w in wines:
-    output.append({'wine':w['wine'], 'pairing':w['pairing'], 'description':w['description']})
-  print(output)
-  wines_json = jsonify(output)
-  return wines_json
-
-  
-#   keys = [
-#       'wine',
-#       'pairing',
-#       'description'
-#   ]
-#   list_of_dictionaries = [dict(zip(keys, wine)) for wine in wines]
-  
-#   print (list_of_dictionaries)  
-  
-   
-#   return render_template('wines.html', wines=list)  
-#   return jsonify({'result': output[2]["wine"]})
-#   print(output)
-#   return jsonify({'result': output})
-#   return jsonify(list_of_dictionaries)
-
-
 # @app.route('/getwine', methods = ['POST'])
 # def getwine():
 #     wine = request.form['wine']
 #     if wine:
 #         wine_list = wines_collection.wines.tolist()
 #         return jsonify({'winesList': wine })
+
 
 # @app.route('/wines/', methods=['GET'])
 # def get_one_wine():
@@ -131,16 +138,6 @@ def get_all_wines():
 #   else:
 #     output = "No such name"
 #   return jsonify({'result' : output})
-
-# @app.route('/star/', methods=['GET'])
-# def get_one_star(name):
-  
-#   s = star.find_one({'name' : name})
-#   if s:
-#     output = {'name' : s['name'], 'distance' : s['distance']}
-#   else:
-#     output = "No such name"
-#   return jsonify({'result' : output})  
 
 
 # @app.route('/wines')
@@ -155,23 +152,7 @@ def get_all_wines():
 #     # return resp
 
 
-@app.route('/recipe')
-def getrecipe():
-    # wines_data = [x for x in wines_collection.find()]
-    recipe = recipes_collection.find()
-    # resp = dumps(wines[2]['wine'])
-    # resp = dumps(wines)
-    # resp = json_util.dumps(wine for wine in wines)
-
-    resp = json_util.dumps(recipe[1])
- 
-    # return render_template('wines.html', wines=resp)    
-    return resp
-
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-# if __name__ == '__main__':
-#   app.run()
